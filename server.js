d23b00dc66f6bd3ebeb489df2859988f07fa3cdf
LighthawkKnight <lighthawkknight@gmail.com>
2019-01-12 23:27:36 -0800

    Create files

---
+++ server.js
@@ -0,0 +1,56 @@
+// Dependencies
+const express = require("express");
+// const mongojs = require("mongojs");
+// const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+// Require axios and cheerio. This makes the scraping possible
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Handlebars
+app.engine(
+	"handlebars",
+	exphbs({
+		defaultLayout: "main",
+		partialsDir: __dirname + '/views/partials/',
+	})
+);
+
+
+
+console.log("\nr/worldnews Scraper" +
+            "\n*******************\n")
+
+axios.get("https://old.reddit.com/r/worldnews/").then(response => {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("p.title").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = $(element).text();
+
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).children().attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);    
+})
